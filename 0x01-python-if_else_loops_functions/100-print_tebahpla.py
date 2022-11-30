cfeb1e64a2de4983f34925327669d55f32b994ff
EmmaJr9 <111970481+EmmaJr9@users.noreply.github.com>
2022-11-30 13:10:07 +0000

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-strtmp = ""
 for i in reversed(range(97, 123)):
-    if (i % 2) == 0:
-        strtmp += chr(i)
-    else:
-        strtmp += chr(i-32)
-print("{}".format(strtmp), end="")
+    print("{:c}".format(i if (i % 2 == 0) else (i - 32)), end='')
