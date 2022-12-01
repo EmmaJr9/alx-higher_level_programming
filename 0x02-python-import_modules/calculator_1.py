2bb82604a3d41d54190706cc43962f0ad1e41ee3
EmmaJr9 <111970481+EmmaJr9@users.noreply.github.com>
2022-12-01 13:02:58 +0000

    Update calculator_1.py

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 def add(a, b):
     """My addition function
+    
     Args:
         a: first integer
         b: second integer
+        
     Returns:
         The return value. a + b
     """
@@ -11,9 +14,11 @@ def add(a, b):
 
 def sub(a, b):
     """My subtraction function
+    
      Args:
         a: first integer
         b: second integer
+        
     Returns:
         The return value. a - b
      """
@@ -21,9 +26,11 @@ def sub(a, b):
 
 def mul(a, b):
     """My multiplication function
+    
     Args:
         a: first integer
         b: second integer
+        
     Returns:
         The return value. a * b
     """
@@ -32,9 +39,11 @@ def mul(a, b):
 
 def div(a, b):
     """My division function
+    
     Args:
         a: first integer
         b: second integer
+        
     Returns:
         The return value. a / b
     """
