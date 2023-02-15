1861765499636ee56898f4718157ad57d24fda2a
EmmaJr9 <111970481+EmmaJr9@users.noreply.github.com>
2023-02-15 18:10:41 +0000

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,7 @@
 -- Results are ordered by ascending cities.id
 SELECT `id`, `name`
 FROM `cities`
-WHERE `states_id` IN (
+WHERE `state_id` IN (
      SELECT `id`
      FROM  `states`
      WHERE `name` = "California"
