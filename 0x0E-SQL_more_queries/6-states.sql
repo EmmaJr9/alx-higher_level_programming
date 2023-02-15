131af8100dbdbc7f668c169c370be5153b8b3901
EmmaJr9 <111970481+EmmaJr9@users.noreply.github.com>
2023-02-15 17:51:20 +0000

    Create 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Creates the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+       `id` INT UNIQUE NOT NULL AUTO_INCREMENT,
+       `name` VARCHAR(256)
+);
