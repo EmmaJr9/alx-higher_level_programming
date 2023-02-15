10fd17e57e01f6dcf397d3787236abd27f93d690
EmmaJr9 <111970481+EmmaJr9@users.noreply.github.com>
2023-02-15 18:08:37 +0000

    Create 8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Lists all the cities of California
+-- Results are ordered by ascending cities.id
+SELECT `id`, `name`
+FROM `cities`
+WHERE `states_id` IN (
+     SELECT `id`
+     FROM  `states`
+     WHERE `name` = 'California'
+)
+ORDER BY `id`;
